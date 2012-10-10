76fc39860e4555153e3c117f4565d5f67cd3adc9
bootsDevelopers <ewan.valentine@boots.co.uk>
2012-10-10 17:02:21 +0100

    Final

--- application/views/nav_view.php
+++ application/views/nav_view.php
@@ -1,8 +1,15 @@
-<nav>
-<a href="http://evdatacenter.co.uk/boots/account/login">Log-in</a><br>
-<a href="http://evdatacenter.co.uk/boots/account/signup">Request sign-up</a><br>
-<!-- if statement for logged in for logout link -->
-<a href="http://evdatacenter.co.uk/boots/account/logout">Log-out</a><br>
-<!-- if statement for admin log in -->
-<a href="http://evdatacenter.co.uk/boots/admin/dashboard">Admin dashboard</a><br>
-</nav>
+<div class="header-container">
+            <header class="wrapper clearfix">
+                <h1 class="title">h1.title</h1>
+                <nav>
+                    <ul>
+                        <li><a href="http://evdatacenter.co.uk/boots/account/login">Log-in</a></li>
+                        <li><a href="http://evdatacenter.co.uk/boots/account/signup">Request sign-up</a></li>
+                        <li><a href="http://evdatacenter.co.uk/boots/admin/dashboard">Admin dashboard</a></li>
+                        <li><a href="http://evdatacenter.co.uk/boots/account/logout">Log-out</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+<div class="main-container">
+    <div class="main wrapper clearfix">
