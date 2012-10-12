c0664cfd87dde90a95b1590773b041e80ee34d19
bootsDevelopers <ewan.valentine@boots.co.uk>
2012-10-12 16:55:49 +0100

    Home time part 2

--- application/views/search_users_view.php
+++ application/views/search_users_view.php
@@ -7,15 +7,10 @@ echo form_open('search/user_search');
 echo validation_errors();
 
 $type = array(
-	'templates' => 'Template',
-	'layouts' => 'Layout',
-	'components' => 'Component'
-	);
-
-$field = array(
-	'title' => 'Title',
-	'description' => 'Description',
-	'id' => 'ID'
+	'email' => 'E-Mail',
+	'first_name' => 'First name',
+	'last_name' => 'Last name',
+	'company' => 'Company'
 	);
 
 echo form_dropdown('type', $type, 'templates');
@@ -23,14 +18,6 @@ echo form_dropdown('type', $type, 'templates');
 echo form_dropdown('field', $field, 'title');
 
 echo form_input('search_term', 'Search');
-
-$data = array(
-    'name' => 'submit',
-    'id' => 'submit',
-    'value' => 'true',
-    'type' => 'submit',
-    'content' => 'Submit'
-);
 
 echo form_button($data);
 
